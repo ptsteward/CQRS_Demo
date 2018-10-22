16f4c92d8931a46f3582a07dd5548a740eab286b
Joshua Steward <joshua.steward@neudesic.com>
2018-10-22 13:26:43 -0700

    Added sample tests

--- CQRS_Demo.Tests/Tests/AddItemHandlerTests.cs
+++ CQRS_Demo.Tests/Tests/AddItemHandlerTests.cs
@@ -11,16 +11,6 @@ using System.Threading.Tasks;
 
 namespace Tests.Tests
 {
-    [TestFixture]
-    public class SampleQueryMessageTests : AppQueryTests<SampleQueryMessage, int>
-    {
-        protected override IEnumerable<DefaultArgument> DefaultArguments => new[]
-        {
-            new DefaultArgument(nameof(SampleQueryMessage.Arg1), "abc"),
-            new DefaultArgument(nameof(SampleQueryMessage.Arg2), "def"),
-            new DefaultArgument(nameof(SampleQueryMessage.Arg3), 23, skipNullCheck: true)
-        };          
-    }
 
     [TestFixture]
     public class AddItemHandlerTests : DependencyInjectedTests<AddItemHandler>
---
+++ CQRS_Demo.Tests/Tests/SampleQueryMessageTests.cs
@@ -0,0 +1,19 @@
+﻿using CQRS_Demo.DataModel.Messages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Tests
+{
+    [TestFixture]
+    public class SampleQueryMessageTests : AppQueryTests<SampleQueryMessage, int>
+    {
+        protected override IEnumerable<DefaultArgument> DefaultArguments => new[]
+        {
+            new DefaultArgument(nameof(SampleQueryMessage.Arg1), "abc"),
+            new DefaultArgument(nameof(SampleQueryMessage.Arg2), "def"),
+            new DefaultArgument(nameof(SampleQueryMessage.Arg3), 23, skipNullCheck: true)
+        };
+    }
+}
